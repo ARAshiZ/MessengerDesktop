356ce2290db3a8506519511b32536d789d0691a8
yakov <d.yakowkin@yandex.ru>
2026-04-20 20:03:21 +0500

    init repo

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerDesktop.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+
+        #region Fields
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructor
+        public MainViewModel() { }
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
