cd5406500df00666e4ca53aed3e606c1a7ab59ce
yakov <d.yakowkin@yandex.ru>
2026-04-21 22:06:06 +0500

    feat(UI): Добавлен шаблон контакта в списке диалогов

---
+++ Models/ChatUserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerDesktop.Models
+{
+    public class ChatUserModel 
+    {
+        public UserModel User { get; set; }
+        public string LastMessage { get; set; }
+    }
+}
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MessengerDesktop.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
@@ -14,14 +16,32 @@ namespace MessengerDesktop.ViewModels
     {
 
         #region Fields
+
         #endregion
 
         #region Properties
-
+        [ObservableProperty]
+        private ObservableCollection<ChatUserModel> _userList = new();
         #endregion
 
         #region Constructor
-        public MainViewModel() { }
+        public MainViewModel() 
+        { 
+            UserModel user1 = new UserModel();
+            user1.Id = 0;
+            user1.Name = "TestUser1";
+            UserModel user2 = new UserModel();
+            user2.Id = 0;
+            user2.Name = "TestUser2";
+            ChatUserModel chatUser1 = new ChatUserModel();
+            ChatUserModel chatUser2 = new ChatUserModel();
+            chatUser1.User = user1;
+            chatUser1.LastMessage = "Hello!";
+            chatUser2.User = user2;
+            chatUser2.LastMessage = "Bye.";
+            UserList.Add(chatUser1);
+            UserList.Add(chatUser2);
+        }
         #endregion
 
         #region Methods
