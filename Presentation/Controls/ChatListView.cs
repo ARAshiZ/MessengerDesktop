5299b209fa597965f7616a0a27ad874fdd75eb2d
yakov <d.yakowkin@yandex.ru>
2026-06-03 20:35:28 +0500

    feat(UI): создан кастомный компонент наследованный от ListView и реализована плавная автопрокрутка

---
+++ Presentation/Controls/ChatListView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MessengerDesktop.Presentation.Controls
+{
+    public class ChatListView : ListView
+    {
+        protected override void OnItemsChanged(NotifyCollectionChangedEventArgs e)
+        {
+            base.OnItemsChanged(e);
+
+            if (e.Action == NotifyCollectionChangedAction.Add) 
+            {
+                ScrollIntoView(Items[Items.Count - 1]);
+            }
+
+        }
+    }
+}
